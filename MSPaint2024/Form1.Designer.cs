7a00cf0ea3105af5800e6dc11696b56f2fdf1745
snejksman <ondrej.black@seznam.cz>
2024-03-06 09:57:54 +0100

    loggr

--- MSPaint2024/Form1.Designer.cs
+++ MSPaint2024/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbPlatno = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ssBottom = new System.Windows.Forms.StatusStrip();
+            this.tsSouradniceMysi = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).BeginInit();
+            this.ssBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbPlatno
+            // 
+            this.pbPlatno.BackColor = System.Drawing.Color.White;
+            this.pbPlatno.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbPlatno.Location = new System.Drawing.Point(12, 12);
+            this.pbPlatno.Name = "pbPlatno";
+            this.pbPlatno.Size = new System.Drawing.Size(952, 538);
+            this.pbPlatno.TabIndex = 0;
+            this.pbPlatno.TabStop = false;
+            this.pbPlatno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbPlatno_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(981, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ssBottom
+            // 
+            this.ssBottom.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsSouradniceMysi});
+            this.ssBottom.Location = new System.Drawing.Point(0, 553);
+            this.ssBottom.Name = "ssBottom";
+            this.ssBottom.Size = new System.Drawing.Size(1237, 22);
+            this.ssBottom.TabIndex = 2;
+            this.ssBottom.Text = "tsSouradniceMysi";
+            this.ssBottom.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            this.ssBottom.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ssBottom_MouseMove);
+            // 
+            // tsSouradniceMysi
+            // 
+            this.tsSouradniceMysi.Name = "tsSouradniceMysi";
+            this.tsSouradniceMysi.Size = new System.Drawing.Size(40, 17);
+            this.tsSouradniceMysi.Text = "x:0 y:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1237, 575);
+            this.Controls.Add(this.ssBottom);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbPlatno);
+            this.Name = "Form1";
+            this.Text = "MS Paint 2024";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlatno)).EndInit();
+            this.ssBottom.ResumeLayout(false);
+            this.ssBottom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbPlatno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip ssBottom;
+        private System.Windows.Forms.ToolStripStatusLabel tsSouradniceMysi;
     }
 }
 
--- MSPaint2024/Form1.cs
+++ MSPaint2024/Form1.cs
@@ -12,9 +12,61 @@ namespace MSPaint2024
 {
     public partial class Form1 : Form
     {
+        // kreslici objekt grafiky - pointer
+        Graphics mobjGrafika;
+         
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //
+        // nahrani okna do pameti
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //vytvoreni grafiky na pictureboxu
+            mobjGrafika = pbPlatno.CreateGraphics();
+        }
+
+        //
+        // test tlacitko  - smazat
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen vtuzka = new Pen(Color.ForestGreen, 3);
+            mobjGrafika.DrawLine(Pens.Aqua, 45, 87, 620, 540);
+
+            mobjGrafika.FillEllipse(Brushes.Bisque, 200, 200, 200, 200);
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ssBottom_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pbPlatno_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                // souradnice mysi do statusu
+                tsSouradniceMysi.Text = "x:" + e.X.ToString();
+                tsSouradniceMysi.Text = "y:" + e.Y.ToString();
+            }
+            catch(Exception ex) 
+            {
+                
+            }
         }
     }
 }
